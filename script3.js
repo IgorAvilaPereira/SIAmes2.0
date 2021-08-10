5ffed8125c9b11bcf0cddc3e47a7f77da87f29ad
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2021-08-10 15:29:29 -0300

    simplificar obs de alteracao multipla

--- script3.js
+++ script3.js
@@ -1,4 +1,4 @@
-alert('[BETA] Datas Previstas == Datas de Realização das Aulas\n1) Acesse o "Diário de Classe Online" da disciplina desejada\n2) Selecione as linhas de um ciclo na tabela do Diário de Classe\n3) Escolha a opção "Alteração Múltipla" \n 4) No SIAmes: insira as atividades deste ciclo na caixa de texto\n5) Clique neste mesmo botão e veja que as datas previstas foram replicadas para as datas das aulas\n7) Basta submeter o formulário do SIA e tá pronto\n8) Agora replique para os demais ciclos!');
+alert('[BETA] Datas Previstas == Datas de Realização das Aulas\n1) Acesse o "Diário de Classe Online" da disciplina desejada\n2) No "Diário" selecione todas as linhas de um mesmo ciclo\n3) Escolha a opção "Alteração Múltipla" \n 4) No SIAmes: insira as atividades deste ciclo na caixa de texto\n5) Clique neste mesmo botão e veja que as datas previstas foram replicadas para as datas das aulas\n7) Basta submeter o formulário do SIA e tá pronto\n8) Agora repita para os demais ciclos!');
 
 var vetDiario = diario.split("<quebra>");
 var qtde = vetDiario.length;
